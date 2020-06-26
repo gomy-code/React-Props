f73d0cf95c6c3a31631ad27fb880f2ad30c76215
gomy-code <jegham63@gmail.com>
2020-06-26 09:34:24 +0100

    chkpt added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import ProductTable from './composant/ProductTable';
 const tableau =[
 { categorie:"electronique",prix:1000,nom:"HP"}
-,{categorie:"maillot",prix:300,nom:"Tommy"}]
+,{categorie:"maillot",prix:300,nom:"Tommy"},
+{ categorie:"electronique",prix:1000,nom:"HP"}]
 
 function App() {
   return (
--- src/composant/ProductTable.js
+++ src/composant/ProductTable.js
@@ -1,25 +1,25 @@
 import React from 'react'
 const ProductTable=(props)=> {
     return(
-        <div>
-            {
-                props.data.map((el,i)=>(
-<table style={{
-    border:'1px solid black'
-}}>
-    <tr>
+        <table style={{
+            border:'1px solid black'
+        }}>
+             <tr>
         <th>Categorie</th>
         <th>Prix</th>
         <th>Nom</th>
     </tr>
-<tr>
+            {
+                props.data.map((el,i)=>(
+
+<tr> 
 <td>{el.categorie}</td>
 <td>{el.prix}</td>
 <td>{el.nom}</td>
 </tr>
-</table>
+
                 ))}
-        </div>
+        </table>
     );
 };
 export default ProductTable
